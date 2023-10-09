de31772b9593df788043202aa2948b336b538138
G00400498@atu.ie <g00400498@atu.ie>
2023-10-09 10:02:21 +0100

    Fixed Data class and attempted switch statement for operators

--- src/main/java/ie/atu/hwcalculator/CalculatorController.java
+++ src/main/java/ie/atu/hwcalculator/CalculatorController.java
@@ -1,4 +1,20 @@
 package ie.atu.hwcalculator;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CalculatorController {
+
+    @GetMapping("/calculate")
+    public CalculatorData calculate(@RequestParam int num1, int num2, String operation) {
+        switch(operation) {
+            case "add" : num1 + num2;
+            case "subtract" : num1 - num2;
+            case "multiply" : num1 * num2;
+            case "divide" : num1 / num2;
+            default : "Not valid";
+        }
+    }
 }
--- src/main/java/ie/atu/hwcalculator/CalculatorData.java
+++ src/main/java/ie/atu/hwcalculator/CalculatorData.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CalculatorData {
-    private float result;
-    private String total;
+    private float total;
+    private String operation;
 }
