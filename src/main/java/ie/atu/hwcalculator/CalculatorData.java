7b7ca1311e50ad94e8d1bfb07154d3d34540a593
G00400498@atu.ie <g00400498@atu.ie>
2023-10-09 09:46:57 +0100

    Getters and setters

---
+++ src/main/java/ie/atu/hwcalculator/CalculatorData.java
@@ -0,0 +1,13 @@
+package ie.atu.hwcalculator;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CalculatorData {
+    private float result;
+    private String total;
+}
